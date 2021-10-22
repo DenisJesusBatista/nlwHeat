4e127d2541fef9803485bae4717262d17ffa672b
Denis Batista <denis.si2011@gmail.com>
2021-10-22 12:41:21 -0300

    FIX: Configurando user.

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import prismaClient from "../prisma";
+import { sign } from "jsonwebtoken"
 
 /**
  * Receber code(string)
@@ -52,11 +53,40 @@ class AuthenticateUserService {
 
         const { login, id, avatar_url, name } = response.data;
 
-        const user = await prismaClient.
+        let user = await prismaClient.user.findFirst({
+            where: {
+                github_id: id
+            }
+        })
+
+        if (!user) {
+            user = await prismaClient.user.create({
+                data: {
+                    github_id: id,
+                    login,
+                    avatar_url,
+                    name
+                }
+            })
+        }
+
+        const token = sign({
+            user: {
+                name: user.name,
+                avatar_url: user.avatar,
+                id: user.id,
+            }
+        },
+            process.env.JWT_SECRET,
+            {
+                subject: user.id,
+                expiresIn: "1d"
+            }
+        )
 
 
 
-        return response.data;
+        return { token, user };
 
 
     }
