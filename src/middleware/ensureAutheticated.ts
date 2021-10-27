bd6edd783fdd2a24c3ea4cb9237de0385c26885f
Denis Batista <denis.si2011@gmail.com>
2021-10-27 19:06:55 -0300

    Validadando Token.

--- src/middleware/ensureAutheticated.ts
+++ src/middleware/ensureAutheticated.ts
@@ -2,6 +2,10 @@ import { Request, Response, NextFunction } from "express"
 import { verify } from "jsonwebtoken";
 
 
+interface IPayload {
+    sub: string;
+}
+
 export function ensureAutheticated(request: Request, response: Response, next: NextFunction) {
     const authToken = request.headers.authorization;
 
@@ -15,7 +19,11 @@ export function ensureAutheticated(request: Request, response: Response, next: N
     const [, token] = authToken.split(" ")
 
     try {
-        const { sub } = verify(token, process.env.JWT_SECRET)
+        const { sub } = verify(token, process.env.JWT_SECRET) as IPayload
+
+        request.user_id = sub;
+
+        return next();
 
     } catch (error) {
         return response.status(401).json({
