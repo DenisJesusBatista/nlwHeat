004cb8efa0bf30d6fa7bb33f381eb0b8c3187eb7
Denis Batista <denis.si2011@gmail.com>
2021-11-18 08:45:25 -0300

    Correcao headers AuthenticateService.

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -32,18 +32,19 @@ class AuthenticateUserService {
 
         const url = "https://github.com/login/oauth/access_token";
 
-        const { data: accessTokenResponse } = await axios.post<IAccessTokenResponse>(url, null, {
+        const { data: accessTokenResponse } =
+            await axios.post<IAccessTokenResponse>(url, null, {
 
-            params: {
-                client_id: process.env.GITHUB_CLIENT_ID,
-                client_secret: process.env.GITHUB_CLIENT_SECRET,
-                code,
-            },
-            headers: {
-                "Accept": "application/json"
-            }
+                params: {
+                    client_id: process.env.GITHUB_CLIENT_ID,
+                    client_secret: process.env.GITHUB_CLIENT_SECRET,
+                    code,
+                },
+                headers: {
+                    Accept: "application/json"
+                },
 
-        });
+            });
 
         const response = await axios.get<IUserResponse>("https://api.github.com/user", {
             headers: {
